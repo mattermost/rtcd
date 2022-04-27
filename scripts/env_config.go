dfd07788e4b7e429b9d1deb76aa7c27b70de3aaa
streamer45 <cstcld91@gmail.com>
2022-04-27 12:23:27 +0200

    Fix env vars document generation

--- scripts/env_config.go
+++ scripts/env_config.go
@@ -1,3 +1,6 @@
+// Copyright (c) 2022-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
 package main
 
 import (
@@ -20,6 +23,12 @@ func main() {
 		log.Fatalf("failed to write file: %s", err.Error())
 	}
 	defer outFile.Close()
+	if err := outFile.Truncate(0); err != nil {
+		log.Fatalf("failed to truncate file: %s", err.Error())
+	}
+	if _, err := outFile.Seek(0, 0); err != nil {
+		log.Fatalf("failed to seek file: %s", err.Error())
+	}
 	fmt := "### Config Environment Overrides\n\n```\nKEY	TYPE\n{{range .}}{{usage_key .}}	{{usage_type .}}\n{{end}}```\n"
 	tabs := tabwriter.NewWriter(outFile, 1, 0, 4, ' ', 0)
 	_ = envconfig.Usagef("rtcd", &service.Config{}, tabs, fmt)
