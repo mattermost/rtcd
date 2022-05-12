4c549b6337f096f530436b5a14efd228af269d20
Claudio Costa <cstcld91@gmail.com>
2022-05-12 16:34:28 +0200

    Print version info on service startup (#32)

--- service/version.go
+++ service/version.go
@@ -6,14 +6,41 @@ package service
 import (
 	"encoding/json"
 	"net/http"
+	"runtime"
 
 	"github.com/mattermost/mattermost-server/v6/shared/mlog"
 )
 
 var (
-	version   = "0.1.0"
-	buildHash string
+	buildVersion string
+	buildHash    string
+	buildDate    string
 )
+
+type versionInfo struct {
+	BuildDate    string `json:"buildDate"`
+	BuildVersion string `json:"buildVersion"`
+	BuildHash    string `json:"buildHash"`
+	GoVersion    string `json:"goVersion"`
+}
+
+func getVersionInfo() versionInfo {
+	return versionInfo{
+		BuildDate:    buildDate,
+		BuildVersion: buildVersion,
+		BuildHash:    buildHash,
+		GoVersion:    runtime.Version(),
+	}
+}
+
+func (v versionInfo) logFields() []mlog.Field {
+	return []mlog.Field{
+		mlog.String("buildDate", v.BuildDate),
+		mlog.String("buildVersion", v.BuildVersion),
+		mlog.String("buildHash", v.BuildHash),
+		mlog.String("goVersion", v.GoVersion),
+	}
+}
 
 func (s *Service) getVersion(w http.ResponseWriter, req *http.Request) {
 	if req.Method != http.MethodGet {
@@ -21,13 +48,8 @@ func (s *Service) getVersion(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	version := map[string]string{
-		"version": version,
-		"build":   buildHash,
-	}
-
 	w.Header().Add("Content-Type", "application/json")
-	if err := json.NewEncoder(w).Encode(version); err != nil {
+	if err := json.NewEncoder(w).Encode(getVersionInfo()); err != nil {
 		s.log.Error("failed to encode data", mlog.Err(err))
 	}
 }
