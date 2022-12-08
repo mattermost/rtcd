0cd37fd2350206fb564213142310067a5a58f10d
streamer45 <cstcld91@gmail.com>
2022-12-08 11:30:34 -0600

    Set config defaults before attempting to load from config file

--- cmd/rtcd/config.go
+++ cmd/rtcd/config.go
@@ -20,9 +20,11 @@ import (
 // variables corresponding to a specific setting.
 func loadConfig(path string) (service.Config, error) {
 	var cfg service.Config
+
+	cfg.SetDefaults()
+
 	if _, err := os.Stat(path); errors.Is(err, os.ErrNotExist) {
 		log.Printf("config file not found at %s, using defaults", path)
-		cfg.SetDefaults()
 	} else if _, err := toml.DecodeFile(path, &cfg); err != nil {
 		return cfg, fmt.Errorf("failed to decode config file: %w", err)
 	}
