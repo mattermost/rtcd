6098bf886ea9f16c4f2ca199d6d2ff766b20f17f
streamer45 <cstcld91@gmail.com>
2023-01-25 17:18:12 -0600

    Respond to ping messages on datachannel

--- service/rtc/sfu.go
+++ service/rtc/sfu.go
@@ -219,6 +219,17 @@ func (s *Server) InitSession(cfg SessionConfig, closeCb func() error) error {
 		}
 	})
 
+	peerConn.OnDataChannel(func(dc *webrtc.DataChannel) {
+		dc.OnMessage(func(msg webrtc.DataChannelMessage) {
+			if string(msg.Data) == "ping" {
+				if err := dc.SendText("pong"); err != nil {
+					s.log.Error("failed to send message", mlog.Err(err))
+					return
+				}
+			}
+		})
+	})
+
 	peerConn.OnTrack(func(remoteTrack *webrtc.TrackRemote, receiver *webrtc.RTPReceiver) {
 		streamID := remoteTrack.StreamID()
 		trackType := remoteTrack.Codec().MimeType
