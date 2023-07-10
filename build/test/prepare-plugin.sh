227f09bd2c2f639400529e5ee2ff0ec44b050166
streamer45 <cstcld91@gmail.com>
2023-07-10 12:42:05 -0600

    Fix CI

--- build/test/prepare-plugin.sh
+++ build/test/prepare-plugin.sh
@@ -12,16 +12,20 @@ else
   echo "Remote branch not found, using default"
   GIT_BRANCH=${GIT_DEFAULT_BRANCH}
 fi
-git clone -b ${GIT_BRANCH} https://github.com/mattermost/mattermost-plugin-calls --depth 1
-cd mattermost-plugin-calls
-cd standalone && npm ci && cd -
-cd webapp && npm ci && cd -
-echo "replace github.com/mattermost/rtcd => ../" >> go.mod
-go mod tidy
-make dist MM_SERVICESETTINGS_ENABLEDEVELOPER=true && cd -
-PLUGIN_BUILD_PATH=$(realpath mattermost-plugin-calls/dist/*.tar.gz)
+
+# Build
+cd .. && git clone -b ${GIT_BRANCH} https://github.com/mattermost/mattermost-plugin-calls --depth 1 && \
+cd mattermost-plugin-calls &&
+cd standalone && npm ci && cd .. && \
+cd webapp && npm ci && cd .. && \
+echo 'replace github.com/mattermost/rtcd => ../rtcd' >> go.mod && \ # We need to make sure we compile the plugin with the rtcd changes.
+go mod tidy && \
+make dist MM_SERVICESETTINGS_ENABLEDEVELOPER=true
+
+# Installation
+PLUGIN_BUILD_PATH=$(realpath dist/*.tar.gz)
 PLUGIN_FILE_NAME=$(basename ${PLUGIN_BUILD_PATH})
-docker cp ${PLUGIN_BUILD_PATH} mmserver_server_1:/mattermost
-docker exec mmserver_server_1 bin/mmctl --local plugin delete ${PLUGIN_ID}
-docker exec mmserver_server_1 bin/mmctl --local plugin add ${PLUGIN_FILE_NAME}
+docker cp ${PLUGIN_BUILD_PATH} mmserver_server_1:/mattermost && \
+docker exec mmserver_server_1 bin/mmctl --local plugin delete ${PLUGIN_ID} && \
+docker exec mmserver_server_1 bin/mmctl --local plugin add ${PLUGIN_FILE_NAME} && \
 docker exec mmserver_server_1 bin/mmctl --local plugin enable ${PLUGIN_ID}
