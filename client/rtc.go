dfee16c39f3bda481f0e7a3df1f95b5bb1cd479f
streamer45 <cstcld91@gmail.com>
2024-08-14 12:25:10 +0200

    Send PLI request on receiving screen track

--- client/rtc.go
+++ client/rtc.go
@@ -14,6 +14,7 @@ import (
 	"sync/atomic"
 
 	"github.com/pion/interceptor"
+	"github.com/pion/rtcp"
 	"github.com/pion/webrtc/v3"
 )
 
@@ -240,6 +241,13 @@ func (c *Client) initRTCSession() error {
 				c.log.Error("failed to stop receiver", slog.String("err", err.Error()))
 			}
 			return
+		}
+
+		if trackType == TrackTypeScreen {
+			c.log.Debug("sending PLI request for received screen track", slog.String("trackID", track.ID()), slog.Any("SSRC", track.SSRC()))
+			if err := pc.WriteRTCP([]rtcp.Packet{&rtcp.PictureLossIndication{MediaSSRC: uint32(track.SSRC())}}); err != nil {
+				c.log.Error("failed to write RTCP packet", slog.String("err", err.Error()))
+			}
 		}
 
 		c.mut.Lock()
